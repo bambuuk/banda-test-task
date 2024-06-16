70eea9eab2d0a5d19c22a390ece69675c1e4bc9f
ivan_korobka <vanyok.korobka@ukr.net>
2024-06-16 18:38:14 +0300

    create FavoriteBtn and use it in Posts page

--- src/components/FavoriteBtn.tsx
+++ src/components/FavoriteBtn.tsx
@@ -1,29 +1,33 @@
-// import React from "react";
+import { useAppSelector } from "@/hooks/useAppSelector";
+import useHandleToggleFavorite from "@/hooks/useHandleToggleFavorite";
+import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
 
-// export const FavoriteBtn = () => {
-//   return (
-//     <button
-//       onClick={() => handleToggleFavorite(post.id)}
-//       className="group p-2 transition-colors border-solid border-2 rounded-md lg:border-[#fff] lg:hover:border-[#dc2626]"
-//       style={{
-//         borderColor: favorites.includes(post.id) ? "#dc2626" : "#fff",
-//       }}
-//     >
-//       {favorites.includes(post.id) ? (
-//         <div className="flex gap-2 sm:gap-4">
-//           <MdFavorite size={30} color="#dc2626" />
-//           <span className="text-[#dc2626] lg:text-[#fff] lg:group-hover:text-[#dc2626] transition-colors">
-//             Remove from Favorites
-//           </span>
-//         </div>
-//       ) : (
-//         <div className="flex gap-4 items-center">
-//           <MdFavoriteBorder size={30} color="#dc2626" />
-//           <span className="text-[#fff] lg:group-hover:text-[#dc2626] transition-colors">
-//             Add to Favorites
-//           </span>
-//         </div>
-//       )}
-//     </button>
-//   );
-// };
+export const FavoriteBtn = ({ id }: { id: number }) => {
+  const favorites = useAppSelector((state) => state.favorites);
+  const handleToggleFavorite = useHandleToggleFavorite();
+  return (
+    <button
+      onClick={() => handleToggleFavorite(id)}
+      className={`group p-2 transition-colors border-solid border-2 rounded-md
+        border-[#fff] ${
+          favorites.includes(id) ? "border-statusRed" : "border-[#fff]"
+        } lg:hover:border-[#dc2626]`}
+    >
+      {favorites.includes(id) ? (
+        <div className="flex gap-2 sm:gap-4">
+          <MdFavorite size={30} color="#dc2626" />
+          <span className="text-[#dc2626] lg:text-[#fff] lg:group-hover:text-[#dc2626] transition-colors">
+            Remove from Favorites
+          </span>
+        </div>
+      ) : (
+        <div className="flex gap-4 items-center">
+          <MdFavoriteBorder size={30} color="#dc2626" />
+          <span className="text-[#fff] lg:group-hover:text-[#dc2626] transition-colors">
+            Add to Favorites
+          </span>
+        </div>
+      )}
+    </button>
+  );
+};
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { useAppSelector } from "@/hooks/useAppSelector";
 import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
 import useHandleToggleFavorite from "@/hooks/useHandleToggleFavorite";
+import { FavoriteBtn } from "./FavoriteBtn";
 
 export const Posts = () => {
   const { data, error, isLoading } = useGetAllPostsQuery("");
@@ -34,31 +35,7 @@ export const Posts = () => {
                         {post.initials}
                       </div>
 
-                      <button
-                        onClick={() => handleToggleFavorite(post.id)}
-                        className={`group p-2 transition-colors border-solid border-2 rounded-md
-                           border-[#fff] ${
-                             favorites.includes(post.id)
-                               ? "border-statusRed"
-                               : "border-[#fff]"
-                           } lg:hover:border-[#dc2626]`}
-                      >
-                        {favorites.includes(post.id) ? (
-                          <div className="flex gap-2 sm:gap-4">
-                            <MdFavorite size={30} color="#dc2626" />
-                            <span className="text-[#dc2626] lg:text-[#fff] lg:group-hover:text-[#dc2626] transition-colors">
-                              Remove from Favorites
-                            </span>
-                          </div>
-                        ) : (
-                          <div className="flex gap-4 items-center">
-                            <MdFavoriteBorder size={30} color="#dc2626" />
-                            <span className="text-[#fff] lg:group-hover:text-[#dc2626] transition-colors">
-                              Add to Favorites
-                            </span>
-                          </div>
-                        )}
-                      </button>
+                      <FavoriteBtn id={post.id} />
                     </div>
                     <Link
                       href={`/posts/${post.id}`}
