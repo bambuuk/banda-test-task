4aef05b3ae7492a8c4faff2b4a4043e5fa2cc29f
ivan_korobka <vanyok.korobka@ukr.net>
2024-06-16 20:49:50 +0300

    add metadata to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center flex-auto">
