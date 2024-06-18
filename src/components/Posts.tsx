b916dc922d563b0df9857cc6b9bd7aad66afc512
ivan_korobka <vanyok.korobka@ukr.net>
2024-06-18 16:33:48 +0300

    refactoring posts query on the Posts page

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,45 +1,35 @@
-"use client";
-
-import { useGetAllPostsQuery } from "@/api/postsApi";
-import { Post } from "@/types/Post";
 import Link from "next/link";
+import { Post } from "@/types/Post";
 import { FavoriteBtn } from "./FavoriteBtn";
 import Avatar from "./Avatar";
+import { getAllPosts } from "@/services/getData";
 
-export const Posts = () => {
-  const { data, error, isLoading } = useGetAllPostsQuery("");
+export const Posts = async () => {
+  const data = await getAllPosts();
 
   return (
     <>
-      {isLoading ? (
-        <div className="mt-10 text-xl font-semibold">Loading ...</div>
-      ) : error ? (
-        <div className="mt-10 text-xl font-semibold">
-          Oooops!! Loading error
-        </div>
-      ) : (
-        <ul className="mt-12 flex flex-col gap-8">
-          {data
-            ? data.map((post: Post) => (
-                <li key={post.id}>
-                  <div className="flex flex-col">
-                    <div className="flex gap-5 items-center">
-                      <Avatar color={post.color} initials={post.initials} />
+      <ul className="mt-12 flex flex-col gap-8">
+        {data
+          ? data.map((post: Post) => (
+              <li key={post.id}>
+                <div className="flex flex-col">
+                  <div className="flex gap-5 items-center">
+                    <Avatar color={post.color} initials={post.initials} />
 
-                      <FavoriteBtn id={post.id} />
-                    </div>
-                    <Link
-                      href={`/posts/${post.id}`}
-                      className="text-xl lg:hover:underline mt-3 transition-all"
-                    >
-                      {post.title}
-                    </Link>{" "}
+                    <FavoriteBtn id={post.id} />
                   </div>
-                </li>
-              ))
-            : ""}
-        </ul>
-      )}
+                  <Link
+                    href={`/posts/${post.id}`}
+                    className="text-xl lg:hover:underline mt-3 transition-all"
+                  >
+                    {post.title}
+                  </Link>{" "}
+                </div>
+              </li>
+            ))
+          : ""}
+      </ul>
     </>
   );
 };
